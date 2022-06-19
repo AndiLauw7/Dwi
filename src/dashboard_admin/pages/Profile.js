2001e89b4094ed70f2c34cdbe5cd025cfb5df9a7
hamamullaiqi <hamamullaiqi@gmail.com>
2022-06-19 06:59:23 +0000

    add profile page

--- src/dashboard_admin/pages/Profile.js
+++ src/dashboard_admin/pages/Profile.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import MyPage from '../components/myPage'
+import avatarDummy from "../../assets/img/null.png";
+
 
 export default function Profile() {
   return (
-    <MyPage 
+    <MyPage
       title={"Profile"}
     >
-      <div>Profile</div>
-
+      <h2>Profile</h2>
+      <div className='d-flex justify-content-center align-items-center my-5'>
+        <img src={avatarDummy} className="rounded-circle" width={300} />
+      </div>
+      <div className='text-center'> 
+      <h5>Name Admin</h5>
+      </div>
+      
     </MyPage>
   )
 }
