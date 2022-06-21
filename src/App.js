17c3af339fef05d227fe3c76d44ff37cf7ea386f
ANDI <andi.safariansyah7@gmail.com>
2022-06-22 00:14:47 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,172 +1,132 @@
-import React, { useContext, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
-import { UserContext } from "./context/userContext";
-import Home from "./components/pages/Home";
-import DetailJourney from "./components/pages/DetailJourney";
-import Bookmark from "./components/pages/Bookmark";
-import Profile from "./components/pages/Profile";
-import AddJourney from "./components/pages/AddJourney";
-import UpdateJourney from "./components/pages/UpdateJourney";
-import Dashboard from "./dashboard_admin/index";
-import Login from "./components/login";
+// import React, { useContext, useEffect } from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./App.css";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+//   useLocation,
+//   useNavigate,
+// } from "react-router-dom";
+// import { UserContext } from "./context/userContext";
+// import Home from "./components/pages/Home";
+// import DetailJourney from "./components/pages/DetailJourney";
+// import Bookmark from "./components/pages/Bookmark";
+// import Profile from "./components/pages/Profile";
+// import AddJourney from "./components/pages/AddJourney";
+// import UpdateJourney from "./components/pages/UpdateJourney";
+// import Dashboard from "./dashboard_admin/index";
+// import Login from "./components/login";
 
-//import API
-import { API, setAuthToken } from "./configAPI/api";
-import { TentangSekolah } from "./components/pages/TentangSekolah";
-import { FasilitasSekolah } from "./components/pages/FasilitasSekolah";
-import { Pendaftaraan } from "./components/pages/Pendaftaraan";
-import { FormRegister } from "./components/pages/FormRegister";
-import HomeTitle from "./components/pages/HomeTitle";
+// //import API
+// import { API, setAuthToken } from "./configAPI/api";
+// import { TentangSekolah } from "./components/pages/TentangSekolah";
+// import { FasilitasSekolah } from "./components/pages/FasilitasSekolah";
+// import { Pendaftaraan } from "./components/pages/Pendaftaraan";
+// import { FormRegister } from "./components/pages/FormRegister";
+// import HomeTitle from "./components/pages/HomeTitle";
 
-<<<<<<< HEAD
-=======
-if (localStorage.token) {
-  setAuthToken(localStorage.token);
-}
+// if (localStorage.token) {
+//   setAuthToken(localStorage.token);
+// }
 
->>>>>>> 3970b6682ef8432300314d30a35e659ee811f844
-function App() {
-  const navigate = useNavigate();
-  const location = useLocation();
+// function App() {
+//   const navigate = useNavigate();
+//   const location = useLocation();
 
-  const [state, dispatch] = useContext(UserContext);
-<<<<<<< HEAD
-  const user = state.user;
-=======
-  const user = state.user
+//   const [state, dispatch] = useContext(UserContext);
+//   const user = state.user
 
-  console.log(user.id);
+//   console.log(user.id);
 
-  
+//   useEffect(() => {
+// 		if (localStorage.token) {
+// 			setAuthToken(localStorage.token);
+// 		}
 
+// 		if (!state.isLogin) {
+// 			return navigate("/login");
+// 		} else if(user.role === "admin" || user.role === "kepalasekolah"){
+// 			return navigate("/dashboard");
+// 		} else {
+//       navigate("/")
+//     }
+// 	}, [state]);
 
->>>>>>> 3970b6682ef8432300314d30a35e659ee811f844
+//   always check auth
+//   const checkUser = async () => {
+//     try {
 
-  // useEffect(() => {
-	// 	if (localStorage.token) {
-	// 		setAuthToken(localStorage.token);
-	// 	}
+//         const response = await API.get("/check-auth");
+//         if (response?.status === 404) {
+//           return dispatch({
+//             type: "AUTH_ERROR",
 
-	// 	if (!state.isLogin) {
-	// 		return navigate("/login");
-	// 	} else if(user.role === "admin" || user.role === "kepalasekolah"){
-	// 		return navigate("/dashboard");
-	// 	} else {
-  //     navigate("/")
-  //   }
-	// }, [state]);
+//           });
 
-  // always check auth
-  const checkUser = async () => {
-    try {
-<<<<<<< HEAD
-      const response = await API.get("/check-auth");
-      if (response?.status === 404) {
-        return dispatch({
-          type: "AUTH_ERROR",
-=======
- 
-        const response = await API.get("/check-auth");
-        if (response?.status === 404) {
-          return dispatch({
-            type: "AUTH_ERROR",
+//         }
+//         let payload = response.data.data?.user;
+//         // Get token from local storage
+//         payload.token = localStorage?.token;
 
-          });
+//         console.log(payload);
 
-        }
-        let payload = response.data.data?.user;
-        // Get token from local storage
-        payload.token = localStorage?.token;
+//         // Send data to useContext
 
-        console.log(payload);
+//         dispatch({
+//           type: "USER_SUCCESS",
+//           payload,
+//         });
+//       }
+//       console.log(response);
+//       let payload = response.data.data?.user;
+//       // Get token from local storage
+//       payload.token = localStorage?.token;
 
-        // Send data to useContext
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
-        dispatch({
-          type: "USER_SUCCESS",
-          payload,
->>>>>>> 3970b6682ef8432300314d30a35e659ee811f844
-        });
-      }
-      console.log(response);
-      let payload = response.data.data?.user;
-      // Get token from local storage
-      payload.token = localStorage?.token;
+//   useEffect(() => {
 
-<<<<<<< HEAD
-      console.log(payload);
+//     if(user.id === undefined){
+//       checkUser();
+//       navigate(location.pathname)
+//     } else {
+//       navigate("login")
+//     }
+//   }, []);
 
-      // Send data to useContext
+//   return (
+//     <Routes>
+//       <Route exact path="/" element={<HomeTitle />} />
+//       {/* <Route exact path="/detail-journey/:id" element={<DetailJourney />} />
+//       <Route exact path="/profile/:id" element={<Profile />} />
+//       <Route exact path="/new-journey" element={<AddJourney />} />
+//       <Route exact path="/bookmark/:id" element={<Bookmark />} />
+//       <Route exact path="/update-journey/:id" element={<UpdateJourney />} /> */}
 
-      dispatch({
-        type: "USER_SUCCESS",
-        payload,
-      });
+//       <Route exact path="/tentang-sekolah" element={<TentangSekolah />} />
+//       <Route exact path="/fasilitas-sekolah" element={<FasilitasSekolah />} />
+//       <Route exact path="/info-ppdb" element={<Pendaftaraan />} />
+//       <Route exact path="/form-ppdb" element={<FormRegister />} />
 
-      // Get user data
-=======
->>>>>>> 3970b6682ef8432300314d30a35e659ee811f844
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//       <Route path="/dashboard/*" element={<Dashboard />} />
+//       <Route path="/login" element={<Login />} />
 
-  useEffect(() => {
-<<<<<<< HEAD
-    if (user) {
-      setAuthToken(localStorage.token);
+//       <Route
+//         exact
+//         path="/profile/:idUser/detail-journey/:id"
+//         element={<DetailJourney />}
+//       />
+//       <Route
+//         exact
+//         path="/bookmark/:idUser/detail-journey/:id"
+//         element={<DetailJourney />}
+//       />
+//     </Routes>
+//   );
+// }
 
-      checkUser();
-    } else {
-      navigate(location.pathname);
-=======
-    
-    if(user.id === undefined){
-      checkUser();
-      navigate(location.pathname)
-    } else {
-      navigate("login")
->>>>>>> 3970b6682ef8432300314d30a35e659ee811f844
-    }
-  }, []);
-
-  return (
-    <Routes>
-      <Route exact path="/" element={<HomeTitle />} />
-      {/* <Route exact path="/detail-journey/:id" element={<DetailJourney />} />
-      <Route exact path="/profile/:id" element={<Profile />} />
-      <Route exact path="/new-journey" element={<AddJourney />} />
-      <Route exact path="/bookmark/:id" element={<Bookmark />} />
-      <Route exact path="/update-journey/:id" element={<UpdateJourney />} /> */}
-
-      <Route exact path="/tentang-sekolah" element={<TentangSekolah />} />
-      <Route exact path="/fasilitas-sekolah" element={<FasilitasSekolah />} />
-      <Route exact path="/info-ppdb" element={<Pendaftaraan />} />
-      <Route exact path="/form-ppdb" element={<FormRegister />} />
-
-      <Route path="/dashboard/*" element={<Dashboard />} />
-      <Route path="/login" element={<Login />} />
-
-      <Route
-        exact
-        path="/profile/:idUser/detail-journey/:id"
-        element={<DetailJourney />}
-      />
-      <Route
-        exact
-        path="/bookmark/:idUser/detail-journey/:id"
-        element={<DetailJourney />}
-      />
-    </Routes>
-  );
-}
-
-export default App;
+// export default App;
