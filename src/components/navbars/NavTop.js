7353a56d37edcc36b9666c11c4553675ef5d619a
hamamullaiqi <hamamullaiqi@gmail.com>
2022-06-24 12:49:51 +0000

    update

--- src/components/navbars/NavTop.js
+++ src/components/navbars/NavTop.js
@@ -43,8 +43,8 @@ function NavTop(props) {
               Fasilitas Sekolah
             </Nav.Link>
             <NavDropdown title="Informasi" id="basic-nav-dropdown">
-              <NavDropdown.Item onClick={() => navigate("/registrasi/*")}>
-                Pendaftaran
+              <NavDropdown.Item onClick={() => navigate("/form-ppdb")}>
+                Pendaftarans
               </NavDropdown.Item>
             </NavDropdown>
             <Nav.Link>Kontak</Nav.Link>
