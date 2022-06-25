0428e066b77ae8ec1e09906bf629227900c4246c
hamamullaiqi <hamamullaiqi@gmail.com>
2022-06-25 20:32:43 +0000

    fixing edit

--- src/components/pages/FormPembayaran.js
+++ src/components/pages/FormPembayaran.js
@@ -1,13 +1,64 @@
 import React, { useContext, useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+import { API } from "../../configAPI/api";
 import { UserContext } from "../../context/userContext";
 import { Footer } from "../navbars/Footer";
-
 import NavbarUser from "../navbars/NavbarUser";
-import NavTop from "../navbars/NavTop";
+
+
+const defValue = {
+  nama_lengkap : "",
+  bukti_pembayaran: ""
+}
 
 export const FormPembayaran = () => {
   const [state, dispatch] = useContext(UserContext);
+  const {id} = useParams()
+  const navigate = useNavigate()
+  const [form, setForm] = useState({...defValue})
+  
+  console.log(form.nama_lengkap);
+
+  const handleChange = (e) => {
+		setForm({
+      ...form,
+			[e.target.name]:
+				e.target.type === "file" ? e.target.files : e.target.value,
+		});
+  }
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+
+      const config = {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      };
+
+      const formData = new FormData();
+      // formData.set("iduser", state.user.id);
+      // formData.set("id_registrasi", state.tb_registrasi.id);
+      formData.set(
+        "bukti_pembayaran",
+        form.bukti_pembayaran[0],
+        form.bukti_pembayaran[0].name
+      );
+      formData.set("nama_lengkap", form.nama_lengkap);
+
+      const response = await API.post(`/pembayaran/add`, formData, config);
+      console.log(response);
+      console.log(formData);
+      if (response.status === 200) {
+        alert("Post Masuk Pa eKO");
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <NavbarUser />
